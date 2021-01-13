9646fbc4881a02d98e877b2b21fce7f82be71ff5
WangYeWei <826036140@qq.com>
2021-01-13 11:03:55 +0800

    Update File.js

--- alipay2uniapp/serverless/File.js
+++ alipay2uniapp/serverless/File.js
@@ -4,17 +4,35 @@
  * @WeChat: wj826036
  * @Motto: 求知若渴，虚心若愚
  * @Description: 
- * @LastEditTime: 2021-01-12 18:00:11
+ * @LastEditTime: 2021-01-13 11:02:49
  * @Version: 1.0
  * @FilePath: \alipay2uniapp\alipay2uniapp\serverless\file.js
  */
+
 export default class File {
   constructor(){
 
   }
 
   uploadFile(options) {
-    return uniCloud.uploadFile(options)
+    console.log(options)
+    const filePath = options.filePath
+    const cloudPath = options.cloudPath || './'
+    return new Promise((my_resolve, my_reject) => {
+      return uniCloud.uploadFile({
+        filePath,
+        cloudPath
+      }).then(res => {
+        const uni_res = {
+          fileUrl: res.fileID,
+          filePath: res.filePath
+        }
+        my_resolve(uni_res)
+      }).catch(err => {
+        const uni_err = err
+        my_reject(uni_err)
+      })
+    })
   }
 
   deleteFile() {
